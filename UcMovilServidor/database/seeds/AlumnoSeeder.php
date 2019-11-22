62b153f163ddaf98382551af8fbafdac7326006f
Savio Campos <sak_psx@hotmail.com>
2019-11-21 23:42:38 -0300

    Lista de alumnos

--- UcMovilServidor/database/seeds/AlumnoSeeder.php
+++ UcMovilServidor/database/seeds/AlumnoSeeder.php
@@ -18,7 +18,7 @@ class AlumnoSeeder extends Seeder
       DB::table('alumnos')->insert([
         'id' => '1',
         'ano_ingreso' => '0001/01/01',
-        'nombre' => 'Bastian',
+        'nombre' => 'Pedro Perez Peralta',
         'ano_nacimiento' => '1995',
         'telefono' => '+56999999999',
         'direccion' => 'El campo bien lejos',
@@ -30,7 +30,7 @@ class AlumnoSeeder extends Seeder
       DB::table('alumnos')->insert([
         'id' => '6',
         'ano_ingreso' => '0001/01/01',
-        'nombre' => 'Savioo',
+        'nombre' => 'Savio Campos Albornoz',
         'ano_nacimiento' => '1995',
         'telefono' => '9997878',
         'direccion' => 'Mi casa',
@@ -38,5 +38,51 @@ class AlumnoSeeder extends Seeder
         'apodo'=>'Sak',
         'id_malla'=>'ICI'
       ]);
+      DB::table('alumnos')->insert([
+        'id' => '90',
+        'ano_ingreso' => '0001/01/01',
+        'nombre' => 'Raul Zagal Bernales',
+        'ano_nacimiento' => '1995',
+        'telefono' => '9997878',
+        'direccion' => 'Mi casa',
+        'semestre_actual' => '1',
+        'apodo'=>'Ra',
+        'id_malla'=>'ICI'
+      ]);
+      DB::table('alumnos')->insert([
+        'id' => '91',
+        'ano_ingreso' => '0001/01/01',
+        'nombre' => 'Dieguito Armando Maladroga',
+        'ano_nacimiento' => '1995',
+        'telefono' => '9997878',
+        'direccion' => 'Mi casa',
+        'semestre_actual' => '1',
+        'apodo'=>'Elduro',
+        'id_malla'=>'ICI'
+      ]);
+      DB::table('alumnos')->insert([
+        'id' => '92',
+        'ano_ingreso' => '0001/01/01',
+        'nombre' => 'Sir Velnassar Crowley',
+        'ano_nacimiento' => '1995',
+        'telefono' => '9997878',
+        'direccion' => 'Mi casa',
+        'semestre_actual' => '1',
+        'apodo'=>'Velna',
+        'id_malla'=>'ICI'
+      ]);
+      DB::table('alumnos')->insert([
+        'id' => '93',
+        'ano_ingreso' => '0001/01/01',
+        'nombre' => 'Ramon Valdes Soto',
+        'ano_nacimiento' => '1995',
+        'telefono' => '9997878',
+        'direccion' => 'Mi casa',
+        'semestre_actual' => '1',
+        'apodo'=>'Moncho',
+        'id_malla'=>'ICI'
+      ]);
+
+
     }
 }
